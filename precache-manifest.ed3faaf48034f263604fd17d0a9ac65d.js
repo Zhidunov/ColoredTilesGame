40b2577a8fbc92ff5c3f5f0b52719e4741232f81
Кафка Тамура <mail.for.kafka@gmail.com>
2020-05-25 20:12:05 +0300

    Updates

---
+++ precache-manifest.ed3faaf48034f263604fd17d0a9ac65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22282530913c0b1682ed07e03b655dd5",
+    "url": "/ColoredTilesGame/index.html"
+  },
+  {
+    "revision": "b34c617f7f8242808d7c",
+    "url": "/ColoredTilesGame/static/css/main.7780f305.chunk.css"
+  },
+  {
+    "revision": "c50fa7980e13cd3aeeb8",
+    "url": "/ColoredTilesGame/static/js/2.74982282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ColoredTilesGame/static/js/2.74982282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34c617f7f8242808d7c",
+    "url": "/ColoredTilesGame/static/js/main.29eddc37.chunk.js"
+  },
+  {
+    "revision": "6bfe1199676ef8674d4f",
+    "url": "/ColoredTilesGame/static/js/runtime-main.0ffa8a77.js"
+  }
+]);
